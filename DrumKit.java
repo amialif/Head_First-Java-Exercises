b0c4cd226c91bb9c585087dc3894884562a83ce6
Md Arifur Rahman <44564356+amialif@users.noreply.github.com>
2020-01-31 10:33:35 +0800

    DrumKit and PoolPuzzle Two - 310120201033AM
    DrumKit and PoolPuzzle Two - 310120201033AM

---
+++ DrumKit.java
@@ -0,0 +1,24 @@
+class DrumKit{
+	boolean topHat = true;
+	boolean snare = true;
+	
+	void playSnare(){
+		
+		System.out.println("bang bang ba-bang");
+		
+	}
+	
+	void playTopHat(){
+		System.out.println("ding ding da-ding");
+	}
+}
+
+class DrumKitTestDrive{
+	public static void main(String[] args){
+		
+		DrumKit d = new DrumKit();
+		
+		d.playSnare();
+		d.playTopHat();
+	}
+}
